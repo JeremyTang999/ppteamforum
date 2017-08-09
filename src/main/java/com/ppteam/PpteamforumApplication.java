f497bfb1ffe5296042e327d72be69e9d69f48e22
JeremyTang999 <iamtmh@163.com>
2017-08-09 16:43:55 +0800

    Initial commit

---
+++ src/main/java/com/ppteam/PpteamforumApplication.java
@@ -0,0 +1,12 @@
+package com.ppteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpteamforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpteamforumApplication.class, args);
+	}
+}
